f8c9c99be956918b412b8a0d6197081255075c17
LoboRich <lobo.r0018@gmail.com>
2019-08-08 10:56:35 +0800

    Scaffolds Driver

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_041937) do
+ActiveRecord::Schema.define(version: 2019_08_07_084520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2019_08_07_041937) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "drivers", force: :cascade do |t|
+    t.string "name"
+    t.string "devise_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
