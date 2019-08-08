e78de4bb0aa55099a7eb2f81a67ed2f22ac6ae68
LoboRich <lobo.r0018@gmail.com>
2019-08-08 16:50:16 +0800

    Display datas under a parent table it's child datas

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
+    @variants = Product.find(params['id']).variants
   end
 
   # GET /products/new
