a0dd14b2be0718b8b3d5a61b14ae31245c867254
Ed Salinas <edsalinas7b@yahoo.com>
2019-08-08 11:50:43 +0800

    Seeds all tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,16 @@ xlsx = Roo::Excelx.new("#{Dir.pwd}/supermarket.xlsx", {:expand_merged_ranges => 
 data = xlsx.parse.drop(1) #drop the header on line 1
 
 data.each do |d|
-	merch = Merchant.find_or_create_by!(name: d[0])
+	merchant_name = d[0]
+	category_name = d[1]
+	product_name = d[2]
+	variant_name = d[3]
+	grocery_price = d[4]
+	juan_ride_price = d[5]
+
+	merch = Merchant.find_or_create_by!(name: merchant_name)
+	category = Category.find_or_create_by!(name: category_name, merchant_id: merch.id)
+	product = Product.find_or_create_by!(name: product_name, category_id: category.id)
+	variant = Variant.find_or_create_by!(name: variant_name, product_id: product.id)
+	variant.update!(grocery_price: grocery_price, price: juan_ride_price)
 end
