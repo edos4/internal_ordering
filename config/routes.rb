206188698e93fc24a0b9bb83bbe147fad7425510
LoboRich <lobo.r0018@gmail.com>
2019-08-09 10:33:16 +0800

    Creates data under a parent data

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get '/dashboard' => 'admins#index'
 
   get 'categories/new/:id' => 'categories#new', :as => :new_merchant_category
+  get 'products/new/:id' => 'products#new', :as => :new_category_product
+  get 'variants/new/:id' => 'variants#new', :as => :new_product_variant
   
   root 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
