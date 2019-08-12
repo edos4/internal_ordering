9f1abb32621e168227262185f17dfa21392cc44e
LoboRich <lobo.r0018@gmail.com>
2019-08-12 12:01:25 +0800

    Datatables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require dataTables/jquery.dataTables
+//= require datatables.js
 //= require bootstrap
 //= require js/shards.min.js
 //= require js/demo.min.js
