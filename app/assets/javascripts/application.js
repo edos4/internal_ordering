afd739bb51548db74ca89a9cd7365eb795ac1fa3
LoboRich <lobo.r0018@gmail.com>
2019-08-12 14:38:48 +0800

    Removes extra unused codes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,46 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+// = require jquery
 //= require jquery_ujs
-//= require bootstrap
-//= require js/shards.min.js
-//= require js/demo.min.js
 //= require_tree .
-
-$(document).ready(function(){
-  $('#exportables').DataTable({
-    dom: 'Blfrtip',
-    "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
-    buttons: [
-      {extend: 'excel', 
-      exportOptions: {
-        columns: 'th:not(:last-child)'
-      }
-      },
-      {extend: 'print',
-        orientation: 'landscape',
-        pageSize: 'LEGAL',
-        exportOptions: {
-        columns: 'th:not(:last-child)'
-      },
-        customize: function (win){
-        $(win.document.body).addClass('white-bg');
-        $(win.document.body).css('font-size', '10px');
-        $(win.document.body).find('.actions').css('display', 'none');
-        
-        }
-      }
-    ],
-    });
-    $(".buttons-print").addClass("btn-dt");
-    $(".buttons-html5").addClass("btn-dt");
-    
-  $("table[role='datatable']").each(function(){
-    $(this).DataTable({
-      processing: true,
-      responsive: true       
-    });
-  });
-
-})
