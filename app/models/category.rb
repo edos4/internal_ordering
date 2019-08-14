2281fe4f48e6bfe0074c5e6d1a7c4376a34bad85
LoboRich <lobo.r0018@gmail.com>
2019-08-14 14:35:48 +0800

    Adds mount uploader to models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+	mount_uploader :image, ImgUploader
 	belongs_to :merchant, optional: true
 	has_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+	mount_uploader :image, ImgUploader
 	belongs_to :category
 	has_many :variants
 end
