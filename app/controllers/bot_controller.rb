e6ebba31ff41905534acf96028e9193b239d7ebd
LoboRich <lobo.r0018@gmail.com>
2019-08-30 16:29:15 +0800

    Changes values for sending msg from bot

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -30,7 +30,7 @@ class BotController < ApplicationController
 
     RestClient::Request.new({
       method: :post,
-      url: "https://api.chatfuel.com/bots/5d4d0b8f4c69c80001e289b5/users/#{messenger_id}/send?chatfuel_token=mELtlMAHYqR0BvgEiMq8zVek3uYUK3OJMbtyrdNPTrQB9ndV0fM7lWTFZbM4MZvD&chatfuel_message_tag=CONFIRMED_EVENT_REMINDER&chatfuel_block_name=#{block_name}&msg=#{params}",
+      url: "https://api.chatfuel.com/bots/#{ENV['CHATFUEL_KEY']}/users/#{messenger_id}/send?chatfuel_token=#{CHATFUEL_BOT_ID}&chatfuel_message_tag=CONFIRMED_EVENT_REMINDER&chatfuel_block_name=#{block_name}&#{params}",
       headers: { 
         accept: :json, 
         content_type: :json
