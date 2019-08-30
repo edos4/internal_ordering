60cce176246c92e8b735bfda4c752cead77fe948
Ed Salinas <edsalinas7b@yahoo.com>
2019-08-30 12:14:39 +0800

    Fixes migrations

--- db/migrate/20190824144720_add_fields_to_everydays_table.rb
+++ db/migrate/20190824144720_add_fields_to_everydays_table.rb
@@ -1,6 +1,5 @@
 class AddFieldsToEverydaysTable < ActiveRecord::Migration[5.2]
   def change
-    add_column :everydays, :description, :string
     add_column :everydays, :ev_order, :string
   end
 end
